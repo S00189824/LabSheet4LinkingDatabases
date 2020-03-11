548f42e37c285a3c3e33f8b77d09846d552ae03e
hypensanez12 <46888854+hypensanez12@users.noreply.github.com>
2020-03-11 01:44:07 +0000

    Finished

---
+++ LabSheet4/LabSheet4Ex2/PartialClasses/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSheet4Ex2
+{
+    public partial class Product
+    {
+    }
+}
